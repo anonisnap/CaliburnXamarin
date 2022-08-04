6d9c7b6a60e4305ecf61ad625abd2c97227debc1
Andreas Young Andersen <andreasyoungandersen@gmail.com>
2022-08-04 14:43:33 +0200

    Add project files.

---
+++ CaliburnXamarin/CaliburnXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CaliburnXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CaliburnXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage( )
+        {
+            InitializeComponent( );
+            BindingContext = new ItemDetailViewModel( );
+        }
+    }
+}
