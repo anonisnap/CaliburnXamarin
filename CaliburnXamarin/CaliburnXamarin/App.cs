786b3629dde754b3d19817e03b4a801e4b68217f
anonisnap <42655737+anonisnap@users.noreply.github.com>
2022-08-08 18:13:55 +0200

    Shell has now been Removed

--- CaliburnXamarin/CaliburnXamarin/App.cs
+++ CaliburnXamarin/CaliburnXamarin/App.cs
@@ -4,51 +4,47 @@ using CaliburnXamarin.Views;
 using Unity;
 using Xamarin.Forms;
 
-namespace CaliburnXamarin
-{
-    public class App : FormsApplication
-    {
-        /// <summary>
-        /// <b>Inversion of Control</b> (IoC) Container<br/>
-        /// <br/>
-        /// Used to access a seperate storage area for Classes
-        /// </summary>
-        private readonly IUnityContainer _unityContainer;
+namespace CaliburnXamarin {
+	public class App : FormsApplication {
+		/// <summary>
+		/// <b>Inversion of Control</b> (IoC) Container<br/>
+		/// <br/>
+		/// Used to access a seperate storage area for Classes
+		/// </summary>
+		private readonly IUnityContainer _unityContainer;
 
-        public App(IUnityContainer container)
-        {
-            // Initializes the FormsApplication Framework
-            Initialize( );
+		public App(IUnityContainer container) {
+			// Initializes the FormsApplication Framework
+			Initialize( );
 
-            _unityContainer = container;
+			_unityContainer = container;
 
-            // Registers a NavigationPane as a Singleton
-            _unityContainer.RegisterType<NavigationPage>(TypeLifetime.Singleton);
+			// Registers a NavigationPane as a Singleton
+			_unityContainer.RegisterType<NavigationPage>(TypeLifetime.Singleton);
 
-            // Register all the Views and ViewModels needed
-            RegisterViewsAndViewModels( );
+			// Register all the Views and ViewModels needed
+			RegisterViewsAndViewModels( );
 
-            // Create the Base View and show the View to the User
-            _ = DisplayRootView<NavigationView>( );
-        }
+			// Create the Base View and show the View to the User
+			//_ = DisplayRootView<NavigationView>( );
+			_ = DisplayRootView<NavigationView>( );
+		}
 
-        /// <summary>
-        /// Registers all needed Views and ViewModels to the IoC Container
-        /// </summary>
-        private void RegisterViewsAndViewModels( )
-        {
-            // Navigation
-            _unityContainer.RegisterType<NavigationViewModel>(TypeLifetime.Singleton, Invoke.Constructor(Resolve.Parameter( )));
-            _unityContainer.RegisterType<NavigationView>(TypeLifetime.Singleton);
+		/// <summary>
+		/// Registers all needed Views and ViewModels to the IoC Container
+		/// </summary>
+		private void RegisterViewsAndViewModels( ) {
+			// Navigation
+			//_unityContainer.RegisterType<NavigationViewModel>(TypeLifetime.Singleton, Invoke.Constructor(Resolve.Parameter( )));
+			//_unityContainer.RegisterType<NavigationView>(TypeLifetime.Singleton);
 
-            // Singleton Counter
-            _unityContainer.RegisterType<SingletonCounterViewModel>(TypeLifetime.Singleton);
-            _unityContainer.RegisterType<SingletonCounterView>(TypeLifetime.Singleton);
-        }
+			// Singleton Counter
+			_unityContainer.RegisterType<SingletonCounterViewModel>(TypeLifetime.Singleton);
+			_unityContainer.RegisterType<SingletonCounterView>(TypeLifetime.Singleton);
+		}
 
-        protected override void PrepareViewFirst(NavigationPage navigationPage)
-        {
-            _unityContainer.RegisterInstance<INavigationService>(new NavigationPageAdapter(navigationPage));
-        }
-    }
+		protected override void PrepareViewFirst(NavigationPage navigationPage) {
+			_unityContainer.RegisterInstance<INavigationService>(new NavigationPageAdapter(navigationPage));
+		}
+	}
 }
--- CaliburnXamarin/CaliburnXamarin/ViewModels/NavigationViewModel.cs
+++ CaliburnXamarin/CaliburnXamarin/ViewModels/NavigationViewModel.cs
@@ -1,22 +1,25 @@
 ﻿using Caliburn.Micro;
 using Caliburn.Micro.Xamarin.Forms;
 
-namespace CaliburnXamarin.ViewModels
-{
-    class NavigationViewModel : Screen
-    {
+namespace CaliburnXamarin.ViewModels {
+    public class NavigationViewModel : Screen {
         #region Fields
         private readonly INavigationService _navService;
         #endregion
 
         #region Properties
-        public string PageInformation { get; set; }
-        public string NavSimpleCounterButton { get; set; }
-        public string NavSingletonCounterButton { get; set; }
+        public string PageInformation {
+            get; set;
+        }
+        public string NavSimpleCounterButton {
+            get; set;
+        }
+        public string NavSingletonCounterButton {
+            get; set;
+        }
         #endregion
 
-        public NavigationViewModel(INavigationService navigationService)
-        {
+        public NavigationViewModel(INavigationService navigationService) {
             _navService = navigationService;
 
             PageInformation = "This is the Main Navigation Screen";
@@ -25,13 +28,11 @@ namespace CaliburnXamarin.ViewModels
         }
 
         #region User Navigation Methods
-        public async void NavSimpleCounter( )
-        {
+        public async void NavSimpleCounter( ) {
             await _navService.NavigateToViewModelAsync<SimpleCounterViewModel>( );
         }
 
-        public async void NavSingletonCounter( )
-        {
+        public async void NavSingletonCounter( ) {
             Screen newView = IoC.Get<SingletonCounterViewModel>( );
             await _navService.NavigateToViewModelAsync<SingletonCounterViewModel>(newView);
         }
