bb41219babae46165cb6b35b56b2c2c7eab9d303
anonisnap <42655737+anonisnap@users.noreply.github.com>
2022-08-09 20:48:59 +0200

    Minor Changes

--- CaliburnXamarin/CaliburnXamarin/ViewModels/NotesViewModel.cs
+++ CaliburnXamarin/CaliburnXamarin/ViewModels/NotesViewModel.cs
@@ -4,67 +4,66 @@ using CaliburnXamarin.Services;
 using CaliburnXamarin.ViewModels.Popups;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Threading;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace CaliburnXamarin.ViewModels
-{
-	public class NotesViewModel : Screen
-	{
+namespace CaliburnXamarin.ViewModels {
+	public class NotesViewModel : Screen {
 		#region Fields
-		private readonly NavigationService _popNav;
+		private readonly NavigationService _popupNav;
 		#endregion
 		#region Properties
-		public string PageInformation { get; set; }
+		public string PageInformation {
+			get; set;
+		}
 
 		public string NoteCount => $"Notes: {Notes.Count}";
 
-		public string AddNewNoteButton { get; set; }
+		public string AddNewNoteButton {
+			get; set;
+		}
 
-		public string RemoveNoteButton { get; set; }
-		public ObservableCollection<Note> Notes { get; }
+		public string RemoveNoteButton {
+			get; set;
+		}
+		public ObservableCollection<Note> Notes {
+			get;
+		}
 
 
-		public Note SelectedNote { get; set; }
+		public Note SelectedNote {
+			get; set;
+		}
 		#endregion
 
-		public NotesViewModel(NavigationService navigationService, NotesModel model)
-		{
+		public NotesViewModel(NavigationService navigationService, NotesModel model) {
 			PageInformation = "Temporary Notes";
 			AddNewNoteButton = "Add New";
 			RemoveNoteButton = "Remove";
 
-			_popNav = navigationService;
+			_popupNav = navigationService;
 
 			Notes = model.Notes;
 			Notes.CollectionChanged += UpdateNoteCount;
 		}
 
 
-		public async void CreateNewNote( )
-		{
-			//await _popNav.PushAsync();
+		public async void CreateNewNote( ) {
+			// Get the Popup View Model from the IoC Container
 			NewNotePopupViewModel vm = IoC.Get<NewNotePopupViewModel>( );
-			await _popNav.NavigateToPopupViewModelAsync(vm);
 
-			// Notify of changes
-			NotifyOfPropertyChange(( ) => NoteCount);
+			// Open the Popup View
+			await _popupNav.NavigateToPopupViewModelAsync(vm);
 		}
 
-		public async void RemoveSelectedNote( )
-		{
-			// Get the Selected Note
-			if ( SelectedNote == null )
-			{
+		public async void RemoveSelectedNote( ) {
+			// Check the Selected Note
+			if (SelectedNote == null) {
 				return;
 			}
 
 			// Ask User if they are sure
 			bool shouldRemove = await Application.Current.MainPage.DisplayAlert("Remove?", SelectedNote.Title, "Ok", "Cancel");
-
-			if ( !shouldRemove )
-			{
+			if (!shouldRemove) {
 				return;
 			}
 
@@ -73,8 +72,7 @@ namespace CaliburnXamarin.ViewModels
 			SelectedNote = null;
 		}
 
-		public void UpdateNoteCount(object sender, NotifyCollectionChangedEventArgs e)
-		{
+		public void UpdateNoteCount(object sender, NotifyCollectionChangedEventArgs e) {
 			NotifyOfPropertyChange(( ) => NoteCount);
 		}
 	}
--- CaliburnXamarin/CaliburnXamarin/ViewModels/Popups/NewNotePopupViewModel.cs
+++ CaliburnXamarin/CaliburnXamarin/ViewModels/Popups/NewNotePopupViewModel.cs
@@ -1,47 +1,63 @@
 ﻿using Caliburn.Micro;
 using Caliburn.Micro.Xamarin.Forms;
 using CaliburnXamarin.Model;
+using Rg.Plugins.Popup.Services;
 
-namespace CaliburnXamarin.ViewModels.Popups
-{
-	public class NewNotePopupViewModel : Screen
-	{
-		public string NoteTitleInfo { get; set; }
-		public string NoteDescInfo { get; set; }
-		public string BtnAdd { get; set; }
-		public string BtnCancel { get; set; }
+namespace CaliburnXamarin.ViewModels.Popups {
+	public class NewNotePopupViewModel : Screen {
+		public string NoteTitleInfo {
+			get; set;
+		}
+		public string NoteDescInfo {
+			get; set;
+		}
+		public string BtnAdd {
+			get; set;
+		}
+		public string BtnCancel {
+			get; set;
+		}
+		public string ErrorNotice {
+			get; set;
+		}
 
 		// User Inputs
-		public string NoteTitleInput { get; set; }
-		public string NoteDescInput { get; set; }
+		public string NoteTitleInput {
+			get; set;
+		}
+		public string NoteDescInput {
+			get; set;
+		}
 
-		public NewNotePopupViewModel( )
-		{
+		public NewNotePopupViewModel( ) {
 			NoteTitleInfo = "Title";
 			NoteDescInfo = "Description";
 			BtnAdd = "Add";
 			BtnCancel = "Cancel";
 		}
 
-		public async void AddNote( )
-		{
-			Note n = new Note( )
-			{
-				Title = NoteTitleInput,
-				Description = NoteDescInput
-			};
+		public async void AddNote( ) {
+			if (!string.IsNullOrWhiteSpace(NoteTitleInput)) {
+				ErrorNotice = "";
 
-			var model = IoC.Get<NotesModel>( );
-			model.AddNewNote(n);
+				Note n = new Note( ) {
+					Title = NoteTitleInput,
+					Description = NoteDescInput
+				};
 
-			// TODO: Close the Popup
-			//await TryCloseAsync( );
+				var model = IoC.Get<NotesModel>( );
+				model.AddNewNote(n);
+
+				await PopupNavigation.Instance.PopAsync( );
+			} else {
+				ErrorNotice = "You must have a Title";
+			}
+
+			NotifyOfPropertyChange(( ) => ErrorNotice);
 		}
 
-		public async void Cancel( )
-		{
-			// TODO: Close the Popup
-			//await TryCloseAsync( );
+		public async void Cancel( ) {
+			await PopupNavigation.Instance.PopAsync( );
 		}
 	}
 }
