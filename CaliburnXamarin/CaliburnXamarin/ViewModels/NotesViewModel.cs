08a1fddd25a7d8808eab793b29b553b7fc4f0046
Andreas Young Andersen <andreasyoungandersen@gmail.com>
2022-08-09 10:35:18 +0200

    Notes can now be Added and Removed

--- CaliburnXamarin/CaliburnXamarin/ViewModels/NotesViewModel.cs
+++ CaliburnXamarin/CaliburnXamarin/ViewModels/NotesViewModel.cs
@@ -2,27 +2,24 @@
 using CaliburnXamarin.Model;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Xamarin.Forms;
 
 namespace CaliburnXamarin.ViewModels
 {
 	public class NotesViewModel : Screen
 	{
 		#region Properties
-		public string PageInformation
-		{
-			get;
-			set;
-		}
+		public string PageInformation { get; set; }
+
 		public string NoteCount => $"Notes: {Notes.Count}";
-		public string AddNewNoteButton
-		{
-			get;
-			set;
-		}
-		public ObservableCollection<Note> Notes
-		{
-			get;
-		}
+
+		public string AddNewNoteButton { get; set; }
+
+		public string RemoveNoteButton { get; set; }
+
+		public ObservableCollection<Note> Notes { get; }
+
+		public Note SelectedNote { get; set; }
 		#endregion
 
 		public NotesViewModel( )
@@ -30,17 +27,48 @@ namespace CaliburnXamarin.ViewModels
 			Notes = new ObservableCollection<Note>( );
 			PageInformation = "Notes";
 			AddNewNoteButton = "Add New";
+			RemoveNoteButton = "Remove";
 		}
 
-		public void CreateNewNote( )
+		public async void CreateNewNote( )
 		{
+			// Get Message from the User via Prompt
+			string msg = await Application.Current.MainPage.DisplayPromptAsync("New Note", "Write your new note");
+
 			Note n = new Note( )
 			{
-				Message = "New Note"
+				Message = msg
 			};
 
+
+			// Add Note to the Collection
 			Notes.Add(n);
 
+			// Notify of changes
+			NotifyOfPropertyChange(( ) => Notes);
+			NotifyOfPropertyChange(( ) => NoteCount);
+		}
+
+		public async void RemoveSelectedNote( )
+		{
+			// Get the Selected Note
+			if ( SelectedNote == null )
+			{
+				return;
+			}
+
+			// Ask User if they are sure
+			bool shouldRemove = await Application.Current.MainPage.DisplayAlert("Remove?", SelectedNote.Message, "Ok", "Cancel");
+
+			if ( !shouldRemove )
+			{
+				return;
+			}
+
+			// Remove the Note
+			Notes.Remove(SelectedNote);
+
+			// Notify of changes
 			NotifyOfPropertyChange(( ) => Notes);
 			NotifyOfPropertyChange(( ) => NoteCount);
 		}
